2a41f885a382a6482c420aec1e79c2fc3ea037f3
Patello <patrk.ekenberg@gmail.com>
2024-01-04 19:52:08 +0000

    Add test cases for DataParser class
    This commit adds test cases for the DataParser class in the test_parse_data.py file. The test cases include checking the processing of transactions and handling a deficit in the database.

--- test/test_parse_data.py
+++ test/test_parse_data.py
@@ -2,8 +2,9 @@ import pytest
 
 from database_handler import DatabaseHandler
 from add_data import DataAdder, SpecialCases
-from data_parser import DataParser
+from data_parser import DataParser, AssetDeficit
 
+# Create a temporary SQLite database in the tmp_path directory using the small dataset
 @pytest.fixture
 def database_small(tmp_path):
     # Create a temporary SQLite database in the tmp_path directory
@@ -14,19 +15,41 @@ def database_small(tmp_path):
     data_adder.add_data("./test/small_data.csv")
     return DatabaseHandler(db_file)
 
+# Create a temporary SQLite database in the tmp_path directory using the small dataset
+# Special cases are not handled, creating a small deficit in the database
+@pytest.fixture
+def database_small_deficit(tmp_path):
+    # Create a temporary SQLite database in the tmp_path directory
+    db_file = tmp_path / "test_asset_data.db"
+    # Create DataAdder object
+    data_adder = DataAdder(DatabaseHandler(db_file))
+    # Add data to database
+    data_adder.add_data("./test/small_data.csv")
+    return DatabaseHandler(db_file)
+
 def test_data_parser__small(database_small):
     # Create DataParser object
     data_parser = DataParser(database_small)
     # Connect to database and get number of unprocessed transactions
     database_small.connect()
-    unprocessed = database_small.get_db_stats(["Unprocessed"])
+    unprocessed = database_small.get_db_stat("Unprocessed" )
     # Process transactions
     data_parser.process_transactions()
-    # Get number of processed transactions after processing. 
-    processed = database_small.get_db_stats(["Processed"])
     # Check that all previously unprocessed transactions are now processed
-    assert processed["Processed"] == unprocessed["Unprocessed"]
-    # Get new number of unprocessed transactions
-    unprocessed = database_small.get_db_stats(["Unprocessed"])
-    # Check that there are no unprocessed transactions
-    assert unprocessed["Unprocessed"] == 0
+    assert database_small.get_db_stat("Processed" ) == unprocessed
+    assert database_small.get_db_stat("Unprocessed" ) == 0
+
+def test_data_parser__defecit(database_small_deficit):
+    # Create DataParser object
+    data_parser = DataParser(database_small_deficit)
+    # Connect to database and get number of unprocessed transactions
+    database_small_deficit.connect()
+    unprocessed = database_small_deficit.get_db_stat("Unprocessed" )
+    # Process transactions, since there is a deficit, an exception should be raised
+    with pytest.raises(AssetDeficit):
+        data_parser.process_transactions()
+    # Check that all previously unprocessed transactions are still unprocessed
+    assert database_small_deficit.get_db_stat("Processed" ) == 0
+    assert database_small_deficit.get_db_stat("Unprocessed" ) == unprocessed
+
+    
